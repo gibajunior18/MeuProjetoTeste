53d138ccf80b736d7fc37dc2bf97d41772977ce6
Giba Junior <gilbertomj@dcomp.ufs.br>
2018-04-26 00:17:19 +0000

    É a terceira que isso me acontece...

--- src/main/java/br/ufs/dcomp/MeuProjeto/App.java
+++ src/main/java/br/ufs/dcomp/MeuProjeto/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! I'm Gilberto, I have 28 years old and I'm excited to learn about Distributed Systems." );
     }
 }
